ad38051f732e06cb50bbacb5c122d9b42b3796ad
djjoel12 <familledan12@gmail.com>
2026-01-21 16:28:29 +0000

    Initial commit - Gbaka Guides fullstack app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: '../backend/public',
-    emptyOutDir: true
-  },
   server: {
     port: 5173,
     proxy: {
